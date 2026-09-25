855b494d0286e0ba880d326c00cea48f05c28245
Aidan Habedank <aidan.habedank@iotventures.net>
2026-09-25 16:52:52 +1200

    image fns: stack overflow attrib stuff

--- js/lib/imageFns.js
+++ js/lib/imageFns.js
@@ -8,6 +8,9 @@
  * TODO: not entirely certain this works quite right. And is it slow?
  * Could we resize the texture and *then* tint it? Might save a few nanoseconds
  */
+// Source - https://stackoverflow.com/a/4231508
+// Posted by Nathan
+// Retrieved 2026-09-25, License - CC BY-SA 2.5 // TODO: check license
 export const tintImage = (img, tint) => {
     // Create a buffer element to draw based on the Image img
     const buffer = document.createElement('canvas');
@@ -30,4 +33,45 @@ export const tintImage = (img, tint) => {
 
     return buffer
 }
+
+// TODO: Also to consider
+// // Source - https://stackoverflow.com/a/54430947
+// // Posted by Mark Miller, modified by community. See post 'Timeline' for change history
+// // Retrieved 2026-09-25, License - CC BY-SA 4.0
+//
+// const canvas = document.getElementById('canvas');
+// const ctx = canvas.getContext('2d');
+// const image = document.getElementById('source');
+//
+// ctx.filter = 'hue-rotate(120deg) grayscale(10%) brightness(150%)';
+// ctx.drawImage(image, 10, 10, 180, 120);
+
+
+// TODO: also also to consider
+// // Source - https://stackoverflow.com/a/45201094
+// Posted by user1693593, modified by community. See post 'Timeline' for change history
+// Retrieved 2026-09-25, License - CC BY-SA 3.0
+
+// // step 1: draw in original image
+// ctx.globalCompositeOperation = "source-over";
+// ctx.drawImage(img, 0, 0);
+//
+// // step 2: adjust saturation (chroma, intensity)
+// ctx.globalCompositeOperation = "saturation";
+// ctx.fillStyle = "hsl(0," + sat + "%, 50%)";  // hue doesn't matter here
+// ctx.fillRect(0, 0);
+//
+// // step 3: adjust hue, preserve luma and chroma
+// ctx.globalCompositeOperation = "hue";
+// ctx.fillStyle = "hsl(" + hue + ",1%, 50%)";  // sat must be > 0, otherwise won't matter
+// ctx.fillRect(0, 0, c.width, c.height);
+//
+// // step 4: in our case, we need to clip as we filled the entire area
+// ctx.globalCompositeOperation = "destination-in";
+// ctx.drawImage(img, 0, 0);
+//
+// // step 5: reset comp mode to default
+// ctx.globalCompositeOperation = "source-over";
+//
+//
 
